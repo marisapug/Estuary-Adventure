53ac0302077fa156e59cdb530f521751711de589
marisapug <marisap@udel.edu>
2017-04-13 17:23:55 -0400

    Created Shore.java
    the shore is a list of everything currently on the shoreline

---
+++ Game/src/model/Shore.java
@@ -0,0 +1,12 @@
+class Shore {
+  List<Barrier> shore;
+  
+ void addBarr(Barrier b){
+  shore.add(b);
+ }
+ 
+ void remBarr(Barrier b){ //update to make sure every "dead" barrier is being removed from the list
+  shore.remove(b);
+ }
+ 
+ }
