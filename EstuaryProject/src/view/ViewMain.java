d5837baf800acc7f9c492451bf4732923ef999e2
logmuir <loganmuir123@gmail.com>
2017-04-13 09:38:11 -0400

    PROJECT

---
+++ EstuaryProject/src/view/ViewMain.java
@@ -0,0 +1,13 @@
+package view;
+
+public class ViewMain {
+	
+	public static void main(String[] args){
+		
+		StartScreenView startView = new StartScreenView();
+		
+		startView.setVisible(true);
+		
+	}//main
+
+}
