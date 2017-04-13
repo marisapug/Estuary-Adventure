3cbfffff220e70e1b63235e0af6a9d008daad45b
marisapug <marisap@udel.edu>
2017-04-13 17:14:30 -0400

    updated boat
    createBoat creates a boat of random size and direction
    move moves the boat depending on its size and direction

--- Game/src/model/Boat.java
+++ Game/src/model/Boat.java
@@ -1,12 +1,64 @@
 package model;
 
+import java.util.Random;
+
 public class Boat extends GameFigure {
-	String boatType;
-	int size;
-	int direction;
-	int speed;
+	//String boatType;
+	String size; // "small" "medium" or "large"
+	String direction;
+	int speed; // 1 , 3, or 5
+	int x;
+	int y;
+	
+	Boat(String size, String direction, int inc, int xc, int yc){
+		size = s;
+		direction = d;
+		inc = i;
+		x = xc;
+		y = yc;
+	}
+	
+	String getSize(){
+		return size;
+	}
+	
+	String getDirection(){
+		return direction;
+	}
+	
+	int getInc(){
+		return inc;
+	}
+	
+	int getX(){
+		return this.x;
+	}
+	
+	void setX(int xc){
+		this.x == xc
+	}
 	
 	void launchWave(){}
 	
-	void move(){}
+	Boat createBoat(){
+		Random rand = new Random();
+		int n = rand.nextInt(3)+1;
+		if (n == 1){
+			Boat b = new Boat("small", "right", 5, 0, 0)
+		} if (n == 2){
+			Boat b = new Boat("medium", "right", 3, 0, 0)
+		} if (n == 3){
+			Boat b = new Boat("large", "left", 1, 0, 0)
+		}
+	
+	void move(){
+		if (getDirection() == "right"){
+			setX(getX() + getInc());
+		}
+		if (getDirection() == "left"){
+			setX(getX() - getInc());
+		}
+	}
+		
+			
 }
