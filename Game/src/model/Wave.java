4c6f47d0bc8f18c9c4e194795482194816e018b4
marisapug <marisap@udel.edu>
2017-04-13 16:53:31 -0400

    updated wave.java by marisa

--- Game/src/model/Wave.java
+++ Game/src/model/Wave.java
@@ -1,5 +1,33 @@
 package model;
 
+import java.util.Random;
+
 public class Wave {
-	int strength;
+	String strength; // strength can either be "small" "medium" "large"
+	int x;
+	int y;
+	
+Wave(String s, int xc, int yc){
+	strength = s;
+	xc = x;
+	yc = y;
+}
+	
+public String getStrength(){
+	return this.strength;
+}
+	
+public int getX(){
+	return this.x;
+}
+	
+public int getY(){
+	return this.y;
+}
+
+public Wave createWave(Boat b){
+	Wave w = new Wave(b.getSize(), b.getX(), b.getY());
+	return w;
+}
+	
 }
