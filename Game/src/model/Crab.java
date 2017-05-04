1d52e44cf91dd0cd385a88715f65ae6a43eef31d
logmuir <loganmuir123@gmail.com>
2017-05-04 09:57:06 -0400

    added javadocs

--- Game/src/model/Crab.java
+++ Game/src/model/Crab.java
@@ -1,11 +1,26 @@
 package model;
 
+/**
+ * Crab gives the specific information needed for the crab 
+ * that it utilized during the maze game, including the type, health,
+ * age, and direction. Crab is a subclass of GameFigure.
+ * 
+ * @author Logan
+ *
+ */
 public class Crab extends GameFigure {
 	private int type; //0 horshoecrab, 1 bluecrab
 	private int health;
 	private int ageState;
 	private int dir; // north 0, south 1, east 2, west 3
-	
+
+	/**
+	 * Constructer that initializes the health, age, x-location, y-location of the crab for the maze game
+	 * @param h health of the crab
+	 * @param a age of the crab
+	 * @param xl x-location of the crab
+	 * @param yl y-location of the crab
+	 */
 	public Crab(int h, int a, int xl, int yl){
 		health = h;
 		ageState = a;
@@ -14,105 +29,115 @@ public class Crab extends GameFigure {
 		xLoc = xl;
 		yLoc = yl;
 	}
-	
+
+	//Movers
+	/**
+	 * Decrements the x-location of the crab by the current x-increment
+	 */
+	public void moveLeft(){
+		xLoc = xLoc - xIncr;
+	}
+
+	/**
+	 * Increments the x-location of the crab x-increment by the current x-increment
+	 */
+	public void moveRight(){
+		xLoc = xLoc + xIncr;
+	}
+
+	/**
+	 * Decrements the y-location of the crab by the current y-increment
+	 */
+	public void moveUp(){
+		yLoc = yLoc - yIncr;
+	}
+
+	/**
+	 * Increments the y-location of the crab by the current y-increment
+	 */
+	public void moveDown(){
+		yLoc = yLoc + yIncr;
+	}
+
+	/**
+	 * Decrements the health of the crab by 1
+	 */
+	public void loseHealth(){
+		health = health - 1;
+	}
+
+	/**
+	 * Increments the age state of the crab by 1
+	 */
+	public void grow(){
+		ageState = ageState + 1;
+	}
+
 	//Getters
 	public int getXLoc(){
 		return this.xLoc;
 	}
-	
+
 	public int getYLoc(){
 		return this.yLoc;
 	}
-	
+
 	public int getXIncr(){
 		return this.xIncr;
 	}
-	
+
 	public int getYIncr(){
 		return this.yIncr;
 	}
-	
+
 	public int getHealth(){
 		return this.health;
 	}
-	
+
 	public int getAgeState(){
 		return this.ageState;
 	}
-	
+
 	public int getDir(){
 		return this.dir;
 	}
-	
+
 	public int getType(){
 		return this.type;
 	}
-	
-	
+
+
 	//Setters
 	public void setXLoc(int x){
 		this.xLoc = x;
 	}
-	
+
 	public void setYLoc(int y){
 		this.yLoc = y;
 	}
-	
+
 	public void setXIncr(int xi){
 		this.xIncr = xi;
 	}
-	
+
 	public void setYIncr(int yi){
 		this.yIncr = yi;
 	}
-	
+
 	public void setHealth(int h){
 		this.health = h;
 	}
-	
+
 	public void setAgeState(int a){
 		this.ageState = a;
 	}
-	
+
 	public void setDir(int d){
 		this.dir = d;
 	}
-	
+
 	public void setType(int t){
 		this.type = t;
 	}
-	
-	
-	//Movers
-	public void moveLeft(){
-		xLoc = xLoc - xIncr;
-	}
-		
-	public void moveRight(){
-		xLoc = xLoc + xIncr;
-	}
-	
-	public void moveUp(){
-		yLoc = yLoc - yIncr;
-	}
-	
-	public void moveDown(){
-		yLoc = yLoc + yIncr;
-	}
-	
-	public void plantGrass(){
-		
-	}
-	
-	public void die(){
-		
-	}
-	
-	public void loseHealth(){
-		health = health - 1;
-	}
-	
-	public void grow(){
-		ageState = ageState + 1;
-	}
+
 }
--- Game/src/model/Litter.java
+++ Game/src/model/Litter.java
@@ -1,6 +1,13 @@
 package model;
 
-
+/**
+ * Litter gives the information needed for all the litter that is
+ * loaded into the maze game, including type, x-location, y-location, width,
+ * height, and the float-x-increment, which indicates the speed the litter will
+ * move in the maze.
+ * @author Logan
+ *
+ */
 public class Litter {
 	private int type;
 	private int xLoc;
@@ -9,6 +16,13 @@ public class Litter {
 	private static int height;
 	static int floatXIncr = 2;
 	
+	/**
+	 * Constructor that initializes the type, x-location, y-location, width, and height of a
+	 * litter object. The width and height of the litter object are set to 50.
+	 * @param t type of the litter
+	 * @param xLoc x-location of the litter
+	 * @param yLoc y-location of the litter
+	 */
 	public Litter(int t, int xLoc, int yLoc){
 		type = t;
 		this.xLoc = xLoc;
@@ -17,24 +31,41 @@ public class Litter {
 		height = 50;
 	}
 	
-	//moves litter by the given x and y increments
+	/**
+	 * Increments the x and y location of the litter by x-increment
+	 * and y-increment
+	 * @param xIncr the current x-increment of the litter
+	 * @param yIncr the current y-increment of the litter
+	 */
 	public void moveLitter(int xIncr, int yIncr){
 		yLoc = yLoc + yIncr;
 		xLoc = xLoc + xIncr;
 	}
 	
-	//changes litters xLoc
+	/**
+	 * Increments the x-location of the litter by the current float x-increment
+	 */
 	public void floatLitterRight(){
 		xLoc = xLoc + floatXIncr;
 	}
 	
+	/**
+	 * Decrements the x-location of the litter by the current float x-increment
+	 */
 	public void floatLitterLeft(){
 		xLoc = xLoc - floatXIncr;
 	}
 	
-	//checks if something hit the litter
+	/**
+	 * Indicates whether a litter object has the same x-location and y-location of another object given
+	 * the x-location and y-location of the current litter object and the width and height.
+	 * @param xL current x-location of litter object
+	 * @param yL current y-location of litter object
+	 * @param w current width of litter object
+	 * @param h current height of litter object
+	 * @return a boolean indicating whether a litter object has contacted another object in the maze game
+	 */
 	public boolean hitLitter(int xL, int yL, int w, int h){
-		//NEEDS FIXING TO WORK
 		if((
 				(xL > xLoc && xL < xLoc + width)   || (xL + w > xLoc && xL + w < xLoc + width)) &&
 				((yL > yLoc && yL < yLoc + height) || (yL + h > yLoc && yL + h < yLoc + height))
@@ -49,6 +80,7 @@ public class Litter {
 	
 
 	
+	//GETTERS
 	public int getXLoc(){
 		return xLoc;
 	}
