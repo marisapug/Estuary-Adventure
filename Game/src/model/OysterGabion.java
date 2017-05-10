525a71ba921a0aebf4c36fea4fd55f7913e0f5c5
marisapug <marisap@udel.edu>
2017-05-09 20:52:52 -0400

    oyster gabion documentation

--- Game/src/model/OysterGabion.java
+++ Game/src/model/OysterGabion.java
@@ -2,8 +2,22 @@ package model;
 
 import java.util.List;
 
+/**
+ * OysterGabion gives the specific information needed for the oyster gabion
+ * that the crab can place on the shoreline
+ * during the estuary defense game -- this includes, xLoc and yLoc
+ * 
+ * @author Marisa
+ *
+ */
 public class OysterGabion extends Barrier {
-	
+
+/**
+	 * Constructor that initializes the x-location, y-location of the OysterGabion
+	 * All parameters are ints
+	 * @param x, x-location of oyster gabion
+	 * @param y, y-location of oyster gabion
+*/
 	public OysterGabion(int x, int y){
 		xLoc = x;
 		yLoc = y;
