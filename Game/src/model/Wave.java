fb97fe448648b746e319dc27330d6a32fe033f8d
marisapug <marisap@udel.edu>
2017-05-09 20:51:53 -0400

    wave documentation

--- Game/src/model/Wave.java
+++ Game/src/model/Wave.java
@@ -2,6 +2,14 @@ package model;
 
 import java.util.Random;
 
+/**
+ * Wave gives the specific information needed for the waves caused by the boats
+ * during the estuary defense game -- this includes strength, x-location,
+ * y-location, width, height, and a boolean that tells if a wave has hit
+ * 
+ * @author Marisa
+ *
+ */
 public class Wave {
 	private int strength; // 0 = small, 1 = medium, 2 = large
 	private int xLoc;
@@ -9,7 +17,16 @@ public class Wave {
 	private int width;
 	private int height = 10;
 	private boolean hasHit = false;
-
+	
+/**
+	 * Constructor that initializes the x-location, y-location, strength,
+	 * and width of the wave
+	 * All parameters are ints
+	 * @param xc, x-location of wave
+	 * @param yc, y-location of wave
+	 * @param w, width of wave
+	 * @param s, strength of wave
+*/
 	public Wave(int xc, int yc, int s, int w) {
 		strength = s;
 		xLoc = xc;
@@ -17,6 +34,10 @@ public class Wave {
 		width = w;
 	}
 	
+	//move(int speed)
+	/**
+	 * Increments the wave by the speed
+	 */
 	public void move(int speed){
 		yLoc += speed;
 	}
