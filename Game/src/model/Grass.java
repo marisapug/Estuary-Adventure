8d0d13c352013f3f69b272b8202d1be39c8c7589
marisapug <marisap@udel.edu>
2017-05-09 20:43:23 -0400

    grass documentation

--- Game/src/model/Grass.java
+++ Game/src/model/Grass.java
@@ -1,10 +1,22 @@
 package model;
 
-public class Grass {
-	private boolean isHit;
+/**
+ * Grass gives the specific information needed for the grass the crab can plant
+ * during the estuary defense game -- this includes, xLoc and yLoc
+ * 
+ * @author Marisa
+ *
+ */
+public class Grass{
 	private int xLoc;
 	private int yLoc;
-	
+
+/**
+	 * Constructor that initializes the x-location, y-location of the grass
+	 * All parameters are ints
+	 * @param x, x-location of grass
+	 * @param y, y-location of grass
+*/
 	public Grass(int x, int y){
 		xLoc = x;
 		yLoc = y;
