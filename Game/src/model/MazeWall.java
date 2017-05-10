1e8fc578e42473c269e7f2fa02681dfd7b5ffa65
logmuir <loganmuir123@gmail.com>
2017-05-10 12:20:51 -0400

    javadocs

--- Game/src/model/MazeWall.java
+++ Game/src/model/MazeWall.java
@@ -1,5 +1,10 @@
 package model;
 
+/**
+ * The MazeWall class contains the information necessary for each individual wall in the maze game.
+ * @author Logan
+ *
+ */
 public class MazeWall {
 	private int startX;
 	private int startY;
@@ -7,6 +12,14 @@ public class MazeWall {
 	private int endY;
 	private int dir;// 0 = horizontal; 1 = vertical
 	
+	/**
+	 * MazeWall constructor, creates a MazeWall object
+	 * @param sX start x-location of the wall
+	 * @param sY start y-location of the wall
+	 * @param eX end x-location
+	 * @param eY end y-location
+	 * @param d direction of the wall (horizontal or vertical)
+	 */
 	public MazeWall(int sX, int sY, int eX, int eY, int d){
 		startX = sX;
 		startY = sY;
@@ -15,6 +28,11 @@ public class MazeWall {
 		dir = d;
 	}
 	
+	/**
+	 * Moves a given wall's x-location and y-location
+	 * @param xIncr x-increment
+	 * @param yIncr y-increment
+	 */
 	public void moveWall(int xIncr, int yIncr){
 		startX = startX + xIncr;
 		endX = endX + xIncr;
@@ -22,6 +40,14 @@ public class MazeWall {
 		endY = endY + yIncr;
 	}
 	
+	/**
+	 * Checks if given object has hit a MazeWall object
+	 * @param xLoc x-location of given object
+	 * @param yLoc y-location of given object
+	 * @param w width of given object
+	 * @param h height of given object
+	 * @return a boolean indicating if given object has hit a MazeWall object
+	 */
 	public boolean isHittingWall(int xLoc, int yLoc, int w, int h){
 		if(dir == 0){
 			if(
