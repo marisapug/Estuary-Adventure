dd570c9c5905916b159a95aca8effaec2b58e909
marisapug <marisap@udel.edu>
2017-05-09 20:52:20 -0400

    seawall documentation

--- Game/src/model/Seawall.java
+++ Game/src/model/Seawall.java
@@ -1,7 +1,21 @@
 package model;
 
+/**
+ * Seawall gives the specific information needed for the seawall
+ * that the crab can place on the shoreline
+ * during the estuary defense game -- this includes, xLoc and yLoc
+ * 
+ * @author Marisa
+ *
+*/
 public class Seawall extends Barrier {
 	
+/**
+	 * Constructor that initializes the x-location, y-location of the seawall
+	 * All parameters are ints
+	 * @param x, x-location of seawall
+	 * @param y, y-location of seawall
+*/
 	public Seawall(int x, int y){
 		xLoc = x;
 		yLoc = y;
@@ -10,3 +24,4 @@ public class Seawall extends Barrier {
 	
 	
 }
+
