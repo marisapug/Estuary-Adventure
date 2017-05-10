f30ecfb64eea7efbf50e1c15ec5b83a218bfacce
nayling <nayling@udel.edu>
2017-05-10 01:09:50 -0400

    dice animation

--- Game/src/model/DiceGame.java
+++ Game/src/model/DiceGame.java
@@ -8,10 +8,30 @@ public class DiceGame {
 	int numImgs = 20;
 	public String diceStory;
 	int animNum;
+	Die[] dice;
+	public int[] imgNums;
+	boolean[] imgBools;
+	int screenWidth;
+	int screenHeight;
+	int diceWidth;
+	int betweenDice;
+	int betweenStory;
+	int diceStartX;
+	int diceStartY;
+	
+	public DiceGame(int sWidth, int sHeight, int dWidth){
+		dice = new Die[numDice];
+		imgNums = new int[numDice];
+		imgBools = new boolean[numImgs];
+		screenWidth = sWidth;
+		screenHeight = sHeight;
+		diceWidth = dWidth;
+		betweenDice = diceWidth + 10;
+		diceStartX = (screenWidth - (numDice / 2 * diceWidth + (numDice / 2 - 1) * betweenDice)) / 2;
+		diceStartY = (screenHeight - (3 * diceWidth + 2 * betweenStory)) / 2;
 		
-	Die[] dice = new Die[numDice];
-	public int[] imgNums = new int[numDice];
-	boolean[] imgBools = new boolean[numImgs];
+		
+	}
 	
 	//Getters
 	
@@ -31,6 +51,10 @@ public class DiceGame {
 		return animNum;
 	}
 	
+	public Die[] getDice(){
+		return dice;
+	}
+	
 	//Setters
 	public void setAnimNum(int a){
 		animNum = a;
@@ -46,8 +70,17 @@ public class DiceGame {
 	public void setDice() {
 		setImgBools();
 		Random rand = new Random();
+		int dieX = 0;
+		int dieY = 0;
 		for (int i = 0; i < numDice; i++) {
-			Die tempDie = new Die(i);
+			if (i < 3) {
+				dieX = diceStartX + (diceWidth + betweenDice) * i - (diceWidth + betweenDice) / 2;
+				dieY = diceStartY;
+			} else {
+				dieX = diceStartX + (diceWidth + betweenDice) * (i % (numDice / 2));
+				dieY = screenHeight - diceStartY - diceWidth;
+			}
+			Die tempDie = new Die(i, dieX, dieY, diceWidth, screenWidth, screenHeight);
 			int randInt = rand.nextInt(numImgs);
 			while(imgBools[randInt]) {
 				randInt = rand.nextInt(numImgs);
