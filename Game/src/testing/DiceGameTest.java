cddc779345a9d952f7fd000f147d44a67b69ec1b
Rachel Butler <rbutler@udel.edu>
2017-05-15 22:20:08 -0400

    updated bad words

---
+++ Game/src/testing/DiceGameTest.java
@@ -0,0 +1,17 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.*;
+
+public class DiceGameTest {
+
+	@Test
+	public void constructorTest() {
+		DiceGame dg = new DiceGame(100, 200, 10);
+		
+	}
+
+}
